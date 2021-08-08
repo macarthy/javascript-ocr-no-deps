400762746d5ee202aded72d6c7813d6ea174ede8
Justin MacCarthy <justin@rubystars.biz>
2021-08-09 01:46:41 +0700

    Add the test and jotted down  directions

---
+++ main.js
@@ -0,0 +1,32 @@
+
+/*
+*
+*
+* class Image
+*{
+* constructor(w, h, data)
+* {
+*  this.pixels = data.slice();
+*  this.width = w;
+*  this.height = h;
+* }
+*}
+*
+* */
+
+const ocr = (image) => {
+
+    return "857843758475893"
+
+}
+
+
+export {ocr as Default }; 
+
+// dumb idea a 1.40am
+// get bounding boxes 
+// down grad the image into BLACK / WHITE 
+// find contours 
+// masking 2x2 slide and was a neraul net 
+// write a script with imagemagic to geternate model 
+//
